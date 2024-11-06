4c00cd99dc93f04d7144a5fce6098b545a169539
Davide Bonifacio <web@davidebonifacio.it>
2024-11-06 19:16:06 +0100

    filtro

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Form from "./components/Form";
 import Filters from "./components/Filters";
 import TaskContainer from "./components/TaskContainer";
+import TextFilter from "./components/TextFilter";
 // import dummyTasks from "./data/tasks";
 import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
@@ -9,16 +10,21 @@ function App({ savedTasks }) {
   // Inizializzo l'elenco di task con l'elenco di task presenti in localStorage, OPPURE con un array vuoto)
   const [tasks, setTasks] = useState(savedTasks || []);
   const [filter, setFilter] = useState("tutti");
+  const [textFilter, setTextFilter] = useState("");
 
   // filtro in base alla stringa contenuta in filter ("tutti" "rimasti" "completati")
-  let filteredTasks = tasks.filter((task) => {
-    // se filter corrisponde a "completati", ritorno i task con proprietà isCompleted impostata a true
-    if (filter === "completati") return task.isCompleted;
-    // se filter corrisponde a "rimasti", ritorno i task con proprietà isCompleted impostata a false (uso l'operatore NOT (!))
-    if (filter === "rimasti") return !task.isCompleted;
-    // se filter corrisponde a "tutti", allora ritorno true per ogni task dell'array
-    if (filter === "tutti") return true;
-  });
+  let filteredTasks = tasks
+    .filter((task) => {
+      // se filter corrisponde a "completati", ritorno i task con proprietà isCompleted impostata a true
+      if (filter === "completati") return task.isCompleted;
+      // se filter corrisponde a "rimasti", ritorno i task con proprietà isCompleted impostata a false (uso l'operatore NOT (!))
+      if (filter === "rimasti") return !task.isCompleted;
+      // se filter corrisponde a "tutti", allora ritorno true per ogni task dell'array
+      if (filter === "tutti") return true;
+    })
+    .filter((task) => {
+      return task.name.toLowerCase().includes(textFilter.toLowerCase());
+    });
 
   useEffect(() => {
     // salvo i tasks in localStorage
@@ -68,6 +74,7 @@ function App({ savedTasks }) {
       <div className="bg-white max-w-screen-sm mx-auto p-4">
         <Form addTask={addTask} />
         <Filters setFilter={setFilter} />
+        <TextFilter textFilter={textFilter} setTextFilter={setTextFilter} />
         <TaskContainer
           tasks={filteredTasks}
           deleteTask={deleteTask}
